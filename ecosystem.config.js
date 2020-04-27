dfad9eb6fc3ae1c3f58dadb7199adf157e7c9f92
Daniel Matsumoto <daniel.matsumoto@kryptus.com>
2020-04-27 13:15:11 -0300

    added ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps : [{
+    script: 'index.js',
+    args: "LexorPvP_  16 '[14940,14950,14960,14970]'",
+    restart_delay: 5000,
+  }, {
+    script: 'index.js',
+    args: "jpgatto 8 '[14945,14955,14965,14975]'",
+    restart_delay: 5000,
+  }],
+
+};
